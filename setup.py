03db69391754c6930767a8496299f349570a5421
Juan Redondo Pizarro <jrp1996@hotmail.es>
2019-09-21 11:25:00 +0200

    fixes

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 
 setup(name='ctlearn_optimizer',
       version='1.0.0',
-      description='Optimization framework for CTLearn',
-      url='https://github.com/ctlearn-project/ctlearn',
+      description='Optimization framework for CTLearn v0.3.0',
+      url='https://github.com/ctlearn-project/ctlearn_optimizer',
+      author='Juan Alfonso Redondo Pizarro',
       license='BSD-3-Clause',
       packages=['ctlearn_optimizer'],
       package_dir={'ctlearn_optimizer': 'src/ctlearn_optimizer'},
       include_package_data=True,
-      dependencies=[],
-      dependency_links=[],
       zip_safe=False)
